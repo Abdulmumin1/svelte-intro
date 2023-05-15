c3b49679e00f5b485d42db1482449b32265cd610
Abdulmumin Abdulkarim <avdorr12345@gmail.com>
2023-05-15 02:10:20 +0100

    svelte

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,8 @@
+<script>
+  let github_repo = "https://github.com/Abdulmumin1";
+  const visit_github = () => {
+    window.location.href = github_repo;
+  };
+</script>
+
+<button on:click={visit_github}> Visit Github </button>
